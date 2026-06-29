d10d8113b8db53629964481babe1e34d572ca5d3
Matt Yeldalo <matt.yeldalo@outlook.com>
2026-06-29 22:21:58 +1000

    feat: add server better server support

--- src/remote_executer.rs
+++ src/remote_executer.rs
@@ -1,14 +1,6 @@
 trait RemoteExecuter {
-    fn root(&self) -> String;
-    fn uptime(&self, server_index: usize) -> FileContent;
-    fn storage(&self, server_index: usize) -> FileContent;
-    fn revision(&self, server_index: usize) -> FileContent;
-    fn git_log(&self, server_index: usize) -> FileContent;
-    fn git_branch(&self, server_index: usize) -> FileContent;
-    fn status(&self, server_index: usize) -> FileContent;
-    fn releases(&self, server_index: usize) -> FileContent;
-    fn retrieve_info(&self, server_index: usize) -> Result<DisplayInfo, String>;
-    fn get_file_content(&self, file_path: &str, server_index: usize) -> FileContent;
-    fn file_content_list(&self, server_index: usize) -> Vec<FileContent>;
     fn server_list(&self) -> Vec<String>;
+    fn server_host_content(&self, server_index: usize) -> Vec<FileContent>;
+    fn deployment_list(&self, server_index: usize) -> Vec<String>;
+    fn deployment_content(&self, server_index: usize, deployment_index: usize) -> Vec<FileContent>;
 }
